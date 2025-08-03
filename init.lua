e1c2b6673155c82b62cd93f217eab95c66c841ff
Ebone Grant <ebonegrant@Ebones-MBP.home.local>
2025-08-03 11:55:35 -0400

    Fix: removed second menu popup cmp

--- init.lua
+++ init.lua
@@ -116,59 +116,59 @@ require('lazy').setup({
     end,
   },
   -- optional blink completion source for require statements and module annotations
-  {
-    'saghen/blink.cmp',
-    -- optional: provides snippets for the snippet source
-    dependencies = { 'rafamadriz/friendly-snippets' },
-
-    -- use a release tag to download pre-built binaries
-    version = '1.*',
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
-
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-      -- 'super-tab' for mappings similar to vscode (tab to accept)
-      -- 'enter' for enter to accept
-      -- 'none' for no mappings
-      --
-      -- All presets have the following mappings:
-      -- C-space: Open menu or open docs if already open
-      -- C-n/C-p or Up/Down: Select next/previous item
-      -- C-e: Hide menu
-      -- C-k: Toggle signature help (if signature.enabled = true)
-      --
-      -- See :h blink-cmp-config-keymap for defining your own keymap
-      keymap = { preset = 'default' },
-
-      appearance = {
-        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono'
-      },
-
-      -- (Default) Only show the documentation popup when manually triggered
-      completion = { documentation = { auto_show = false } },
-
-      -- Default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, due to `opts_extend`
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
-      },
-
-      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-      --
-      -- See the fuzzy documentation for more information
-      fuzzy = { implementation = "lua" }
-    },
-    opts_extend = { "sources.default" }
-  },
+  -- {
+  --   'saghen/blink.cmp',
+  --   -- optional: provides snippets for the snippet source
+  --   dependencies = { 'rafamadriz/friendly-snippets' },
+  --
+  --   -- use a release tag to download pre-built binaries
+  --   version = '1.*',
+  --   -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+  --   -- build = 'cargo build --release',
+  --   -- If you use nix, you can build from source using latest nightly rust with:
+  --   -- build = 'nix run .#build-plugin',
+  --
+  --   ---@module 'blink.cmp'
+  --   ---@type blink.cmp.Config
+  --   opts = {
+  --     'default', --(recommended) for mappings similar to built-in completions (C-y to accept)
+  --     -- 'super-tab' for mappings similar to vscode (tab to accept)
+  --     -- 'enter' for enter to accept
+  --     -- 'none' for no mappings
+  --     --
+  --     -- All presets have the following mappings:
+  --     -- C-space: Open menu or open docs if already open
+  --     -- C-n/C-p or Up/Down: Select next/previous item
+  --     -- C-e: Hide menu
+  --     -- C-k: Toggle signature help (if signature.enabled = true)
+  --     --
+  --     -- See :h blink-cmp-config-keymap for defining your own keymap
+  --     keymap = { preset = 'default' },
+  --
+  --     appearance = {
+  --       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+  --       -- Adjusts spacing to ensure icons are aligned
+  --       nerd_font_variant = 'mono'
+  --     },
+  --
+  --     -- (Default) Only show the documentation popup when manually triggered
+  --     completion = { documentation = { auto_show = false } },
+  --
+  --     -- Default list of enabled providers defined so that you can extend it
+  --     -- elsewhere in your config, without redefining it, due to `opts_extend`
+  --     sources = {
+  --       default = { 'lsp', 'path', 'snippets', 'buffer' },
+  --     },
+  --
+  --     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+  --     -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+  --     -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+  --     --
+  --     -- See the fuzzy documentation for more information
+  --     fuzzy = { implementation = "lua" }
+  --   },
+  --   opts_extend = { "sources.default" }
+  -- },
   {
     "j-hui/fidget.nvim",
     opts = {
@@ -295,7 +295,7 @@ require("autoclose").setup()
 require('neoscroll').setup({
   mappings = {                 -- Keys to be mapped to their corresponding default scrolling animation
     '<C-u>', '<C-d>',
-    '<C-b>', '<C-f>',
+    --'<C-b>', '<C-f>',
     '<C-y>', '<C-e>',
     'zt', 'zz', 'zb',
   },
@@ -554,10 +554,6 @@ vim.keymap.set("n", "<A-2>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<A-3>", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<A-4>", function() harpoon:list():select(4) end)
 
--- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
-
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
@@ -641,6 +637,7 @@ parser_config.motoko = {
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set('n', '<leader>h', vim.lsp.buf.signature_help, { desc = "Open floating diagnostice mssage" })
 
 -- Enable the following language servers
 vim.lsp.enable('gopls')
@@ -692,6 +689,18 @@ cmp.setup({
     { name = 'buffer' },
   })
 })
+
+local ls = require("luasnip")
+
+vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
+
+vim.keymap.set({"i", "s"}, "<C-E>", function()
+	if ls.choice_active() then
+		ls.change_choice(1)
+	end
+end, {silent = true})
 
 -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
 -- Set configuration for specific filetype.
@@ -756,8 +765,9 @@ vim.bo.completefunc = nil
 local options = { noremap = true }
 vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("i", "jl", "<C-o>")
-vim.keymap.set("n", "<F2>", ":wa<CR>:GoFmt<CR>:GoImports<CR>")
-vim.keymap.set("n", "<F12>", ":GoFmt<CR>:GoImport<CR>")
+vim.keymap.set("n", "<F2>", ":wa<CR>")
+vim.keymap.set("n", "<F10>", ":bd<CR>")
+vim.keymap.set("n", "<F12>", ":wa<CR>:terminal dotnet test<CR>")
 vim.keymap.set("n", "<C-h>", "^")
 vim.keymap.set("n", "<C-l>", "$")
 vim.opt.cursorline = true
